068ea4aeac6def8d9aa5ebee3cfa501af018cd0c
sloymad <sloymad@gmail.com>
2018-02-13 12:27:17 +0300

    initial commit

---
+++ src/main/java/com/company/Square.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+    public double area() {
+
+        return this.l * this.l;
+    }
+}
